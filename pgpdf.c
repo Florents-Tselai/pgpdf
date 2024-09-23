adfacae85a392bfdd6a8b33a9dab1011e5d15975
Florents Tselai <florents.tselai@gmail.com>
2024-09-23 13:23:04 +0300

    Read bytes too

--- pgpdf.c
+++ pgpdf.c
@@ -6,7 +6,7 @@
 #include "poppler/glib/poppler.h"
 
 #if PG_VERSION_NUM >= 160000
-
+#include <glib.h>
 #include "varatt.h"
 
 #endif
@@ -18,30 +18,8 @@
 
 PG_MODULE_MAGIC;
 
-#include <poppler/glib/poppler.h>
-#include <glib.h>
-#include <stdio.h>
-
-
-PG_FUNCTION_INFO_V1(pdf_read_file);
-
-Datum pdf_read_file(PG_FUNCTION_ARGS) {
-    char *pdf_file = text_to_cstring(PG_GETARG_TEXT_PP(0));
-    GError *error = NULL;
-    PopplerDocument *document;
-    gchar *page_text;
-    int num_pages; /* Get the number of pages in the PDF */
-    StringInfo strinfo = makeStringInfo();
-
-    /* Open the PDF document */
-    document = poppler_document_new_from_file(g_strdup_printf("file://%s", pdf_file), NULL, &error);
-    if (!document)
-        elog(ERROR, "Error opening PDF document: %s\n", error->message);
-
-
-    num_pages = poppler_document_get_n_pages(document);
-
-    /* Iterate through pages and extract text */
+static void extract_text_from_pdf(PopplerDocument *document, StringInfo strinfo) {
+    int num_pages = poppler_document_get_n_pages(document);
     for (int i = 0; i < num_pages; i++) {
         PopplerPage *page = poppler_document_get_page(document, i);
         if (!page) {
@@ -49,7 +27,7 @@ Datum pdf_read_file(PG_FUNCTION_ARGS) {
             continue;
         }
 
-        page_text = poppler_page_get_text(page);
+        gchar *page_text = poppler_page_get_text(page);
         if (page_text) {
             appendStringInfo(strinfo, "%s", page_text);
             g_free(page_text);
@@ -59,8 +37,47 @@ Datum pdf_read_file(PG_FUNCTION_ARGS) {
 
         g_object_unref(page);
     }
+}
 
-    /* Close the PDF document */
+static PopplerDocument *open_pdf_file(const char *file_path, GError **error) {
+    return poppler_document_new_from_file(g_strdup_printf("file://%s", file_path), NULL, error);
+}
+
+static PopplerDocument *open_pdf_bytes(bytea *bytes, GError **error) {
+    return poppler_document_new_from_bytes(g_bytes_new(VARDATA_ANY(bytes), VARSIZE_ANY_EXHDR(bytes)), NULL, error);
+}
+
+PG_FUNCTION_INFO_V1(pdf_read_file);
+
+Datum pdf_read_file(PG_FUNCTION_ARGS) {
+    char *pdf_file = text_to_cstring(PG_GETARG_TEXT_PP(0));
+    GError *error = NULL;
+    StringInfo strinfo = makeStringInfo();
+
+    PopplerDocument *document = open_pdf_file(pdf_file, &error);
+    if (!document) {
+        elog(ERROR, "Error opening PDF document: %s\n", error->message);
+    }
+
+    extract_text_from_pdf(document, strinfo);
+    g_object_unref(document);
+
+    PG_RETURN_TEXT_P(cstring_to_text(strinfo->data));
+}
+
+PG_FUNCTION_INFO_V1(pdf_read_bytes);
+
+Datum pdf_read_bytes(PG_FUNCTION_ARGS) {
+    bytea *bytes = PG_GETARG_BYTEA_PP(0);
+    GError *error = NULL;
+    StringInfo strinfo = makeStringInfo();
+
+    PopplerDocument *document = open_pdf_bytes(bytes, &error);
+    if (!document) {
+        elog(ERROR, "Error opening PDF document: %s\n", error->message);
+    }
+
+    extract_text_from_pdf(document, strinfo);
     g_object_unref(document);
 
     PG_RETURN_TEXT_P(cstring_to_text(strinfo->data));
--- sql/pgpdf--0.1.0.sql
+++ sql/pgpdf--0.1.0.sql
@@ -1,3 +1,7 @@
 CREATE FUNCTION pdf_read_file(text) returns text AS 'MODULE_PATHNAME',
 'pdf_read_file'
     LANGUAGE C;
+
+CREATE FUNCTION pdf_read_bytes(bytea) returns text AS 'MODULE_PATHNAME',
+'pdf_read_bytes'
+    LANGUAGE C;
